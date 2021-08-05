361d702087c8ef8e21dfc47c1f752c85d7edbb89
franck <franck_gxu@outlook.com>
2021-08-06 03:35:46 +0800

    添加method练习

---
+++ go_learning/methods/main_method_fuc.go
@@ -0,0 +1,27 @@
+package main
+
+import (  
+    "fmt"
+)
+
+type Employee struct {  
+    name     string
+    salary   int
+    currency string
+}
+
+/*
+ displaySalary() method converted to function with Employee as parameter
+*/
+func displaySalary(e Employee) {  
+    fmt.Printf("Salary of %s is %s%d", e.name, e.currency, e.salary)
+}
+
+func main() {  
+    emp1 := Employee{
+        name:     "Sam Adolf",
+        salary:   5000,
+        currency: "$",
+    }
+    displaySalary(emp1)
+}
