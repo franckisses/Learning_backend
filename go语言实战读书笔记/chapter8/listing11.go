7a4f178e458796210b18f869c217410fb6312301
franck <franck_gxu@outlook.com>
2021-08-17 13:57:21 +0800

    更新地址

--- go语言实战读书笔记/chapter8/listing11.go
+++ go语言实战读书笔记/chapter8/listing11.go
@@ -40,7 +40,7 @@ func init() {
 
 func main() {
 	Trace.Println("I have something standard to say")
-	// Info.Println("Special Information")
-	// Warning.Println("There is something you need to know about")
-	// Error.Println("Something has failed")
+	Info.Println("Special Information")
+	Warning.Println("There is something you need to know about")
+	Error.Println("Something has failed")
 }
