eaf620047a900ef7ad32ed59499dfc792e7d83d7
franck <franck_gxu@outlook.com>
2022-05-08 14:43:34 +0800

    clean folder

---
+++ fluent python/01_python_data_model/vector_1.py
@@ -0,0 +1,46 @@
+from math import hypot
+
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y  
+    
+    def __repr__(self):
+        return 'Vector(%r, %r)' % (self.x, self.y)
+    
+    def __abs__(self):
+        return hypot(self.x, self.y)
+    
+    def __bool__(self):
+        return bool(abs(self))
+    
+    def __bool__(self):
+        return bool(self.x or self.y)   
+    
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+    
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
+
+
+
+command = """
+>>> from vector_1 import Vector
+>>> a = Vector()
+>>> a
+Vector(0, 0)
+>>> abs(a)
+0.0
+>>> bool(a)
+False
+>>> b = Vector(10,11)
+>>> b
+Vector(10, 11)
+>>> a + b
+Vector(10, 11)
+>>> b * 10
+Vector(100, 110)
+"""
