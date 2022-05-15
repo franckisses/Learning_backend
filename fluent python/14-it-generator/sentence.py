f939f013f71dd39e43962d2a2638bb9ef5cb8d84
franck <franck_gxu@outlook.com>
2022-05-16 04:39:18 +0800

    obj reloading

---
+++ fluent python/14-it-generator/sentence.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import re
+import reprlib
+
+RE_WORD = re.compile('\w+')
+
+class Sentence:
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(text)
+
+    def __getitem__(self, index):
+        return self.words[index]
+
+    def __len__(self):
+        return len(self.words)
+
+    def __repr__(self):
+        return 'Sentence(%s)' % reprlib.repr(self.text)
+
+
+"""
+>>> from sentence import *
+>>> s = Sentence('"The time has come," the Walrus said,"')
+>>> s
+Sentence('"The time ha...Walrus said,"')
+>>> for word in s:
+...     print(word)
+...
+The
+time
+has
+come
+the
+Walrus
+said
+>>> list(s)
+['The', 'time', 'has', 'come', 'the', 'Walrus', 'said']
+"""
