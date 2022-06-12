38ac6db09a5aa1e86d24183d5e99143063149afe
franck <franck_gxu@outlook.com>
2022-06-12 14:02:00 +0800

    property

---
+++ fluent python/19_dynmatic/bulkfood_v2prop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class LineItem:
+    weight = quantity('weight')
+    price = quantity('price')
+
+    def __init__(self, description, weight, price):
+        self.description = description
+        self.weight = weight
+        self.price = price
+
+    def subtotal(self):
+        return self.price * self.weight
+
