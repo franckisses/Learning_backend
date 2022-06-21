0d0901b82f769e7c41ec69551926a760d7a097ff
franck <franck_gxu@outlook.com>
2022-06-21 13:52:22 +0800

    描述类

--- fluent python/20_attr_desc/bulkfood_v4.py
+++ fluent python/20_attr_desc/bulkfood_v4.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 
 class Quantity:
+    __counter = 0
+
     def __init__(self, storage_name):
-        self.storage_name = storage_name
+        cls = self.__class__
+        prefix = cls.__name__
+        index = cls.__counter
+        self.storage_name = '{}#{}'.format(prefix, index)
+        cls.__counter += 1
+
+    def __get__(self, instance, owner):
+        return getattr(instance,self.storage_name)
 
     def __set__(self, instance, value):
         if value > 0:
-            instance.__dict__[self.storage_name] = value
+            setattr(instance, self.storage_name, value)
         else:
             raise ValueError('value must be >0')
 
