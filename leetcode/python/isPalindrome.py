d8108f9c18c1dde5e04aa287ffc357117ae7b7f0
franck <franck_gxu@outlook.com>
2023-02-05 19:39:57 +0800

    test

---
+++ leetcode/python/isPalindrome.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# @Time: 2023-02-03
+# @File: %
+# @Author: dreamhomes
+# @Description: 
+
+"""
+给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+
+回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+
+例如，121 是回文，而 123 不是。
+ 
+
+示例 1：
+
+输入：x = 121
+输出：true
+示例 2：
+
+输入：x = -121
+输出：false
+解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+示例 3：
+
+输入：x = 10
+输出：false
+解释：从右向左读, 为 01 。因此它不是一个回文数。
+ 
+
+提示：
+
+-231 <= x <= 231 - 1
+
+"""
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if str(x) == str(x)[::-1]:
+            return True
+        return False
+
+
+if __name__ == '__main__':
+    a = Solution()
+    print(a.isPalindrome(123321))
---
+++ leetcode/python/two_of_sum.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# @Time: 2023-02-03
+# @File: %
+# @Author: dreamhomes
+# @Description: 
+
+"""
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+你可以按任意顺序返回答案。
+
+示例 1：
+
+输入：nums = [2,7,11,15], target = 9
+输出：[0,1]
+解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+示例 2：
+
+输入：nums = [3,2,4], target = 6
+输出：[1,2]
+示例 3：
+
+输入：nums = [3,3], target = 6
+输出：[0,1]
+
+"""
+class Solution:
+    def twoSum(self, nums, target):
+        for x in range(len(nums)-1):
+            for y in range(x+ 1, len(nums)):
+                print(x, y)
+                if nums[x] + nums[y] == target:
+                    return [x,y]
+
+
+if __name__ == '__main__':
+    a = Solution()
+    print(a.twoSum(nums=[2,5,5,11], target=10))
+
